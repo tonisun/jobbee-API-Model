eee61bb79357fc305ff2de51731b405b50b300e2
Toni Zeidler <toni.z@gmx.de>
2023-07-30 15:14:10 +0200

    add serch pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -9,7 +9,13 @@ const APIFilters = require('../utils/apiFilters')
 // Get all Jobs => /api/v1/jobs
 exports.getJobs = catchAsyncErrors( async (req, res, next) => {
 
-    const apiFilters = new APIFilters(Job.find(), req.query).filter().sort()
+    const apiFilters = new APIFilters(Job.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .searchByQuery()
+        .pagination()
+
     const jobs = await apiFilters.query
 
     res.status(200).json({
--- utils/apiFilters.js
+++ utils/apiFilters.js
@@ -13,8 +13,8 @@ class APIFilters {
         let queryStr = JSON.stringify(queryCopy)
         logger.info(`Time: ${new Date().toISOString()}, Query String: ${queryStr}`)  
 
-        // Removing fields from the query
-        const removeFields = ['sort']
+        // Remove fields from the query that are not in the model Job
+        const removeFields = ['sort', 'fields', 'q', 'limit', 'page']
         removeFields.forEach( el => delete queryCopy[el])
 
         /* 
@@ -33,6 +33,7 @@ class APIFilters {
         return this
     }
 
+    // Second Filter
     sort() {
         if (this.queryString.sort) {
             const sortBy = this.queryString.sort.split(',').join(' ')
@@ -42,6 +43,38 @@ class APIFilters {
         } else {
             this.query = this.query.sort('-postingDate')
         }
+        return this
+    }
+
+    // Third Filter
+    limitFields() {
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' ')
+            this.query = this.query.select(fields)
+        } else {
+            // I dont want to see this field in the results
+            this.query = this.query.select('-__v')
+        }
+        return this
+    }
+
+    // Ford Filter Search
+    searchByQuery () {
+        if (this.queryString.q) {
+            const qu = this.queryString.q.split('-').join(' ')
+            this.query = this.query.find({$text: {$search: "\"" + qu + "\""}})
+        }
+        return this
+    }
+
+    // Fifth Filter Pagination
+    pagination () {
+        const page = parseInt(this.queryString.page, 10) || 1
+        const limit = parseInt(this.queryString.limit, 10) || 10
+        const skipResults = ( page - 1 ) * limit
+
+        this.query = this.query.skip(skipResults).limit(limit)
+
         return this
     }
 }
